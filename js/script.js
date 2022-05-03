1494c22d1e8b402171e8a0b3db765f20c69ce395
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-05-03 23:00:51 +0000

    spelling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Stella S All rights reserved
 //
 // Created by: Stella S
-// Created on: March 2022
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -14,22 +14,23 @@ function displayNumber () {
   let answer = ""
   const MIN_R = 18
   const MIN_PG = 13
+  const MIN_G = 10
   
 // get user input
 	let age = parseInt(document.getElementById('age').value)
 
 
-  	// if guess is right says correct
+  	// if age is greater or euqal to 18
 	if (age >= MIN_R){
-		answer = "You can see an R rated movie. ENOJOY, hoped you picked a good one!"
+		answer = "You can see an R rated movie. ENOJOY!"
 	} 
-  	// if guess is right says correct
+  	// if age is greater or equal to 13
 	else if (age >= MIN_PG){
-		answer = "Yay! Your older enough to watch PG-13 rated movie. Have Fun!"
+		answer = "Yay! You're older enough to watch a PG-13 rated movie. Have Fun!"
 	} 
-	//if not says incorrect	
+	//if age is less than 13
 	else {
-		answer = "Congrats! You can go see a G rated movie! 
+		answer = "Congrats! You can go see a G rated movie, with a parent is recommended :)!" 
 	}
 
   // display the results
